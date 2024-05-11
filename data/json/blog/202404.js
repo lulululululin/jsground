283c438cf521cca6d9691f2be9167d1b9c3ec173
luke lin <77534501+sunrenn@users.noreply.github.com>
2024-05-11 21:50:35 +0800

    adj

--- data/json/blog/202404.js
+++ data/json/blog/202404.js
@@ -14,12 +14,12 @@ loglist.push({
     'title':'书籍内容的分层',
     'date':"2024.04.05",
     'log_content':[
-        "在我“处理”一本书的时候，突然想到在英语里是如何给书籍的内容封层的呢？",
+        "在我“处理”一本书的时候，突然想到在英语里是如何给书籍的内容分层的呢？",
         "比如 Chapter 和 section。",
         "上网查了一下，大概是这样的： part / chapter / section / paragraph",
         "然后呢，还有 subpart / subchapter / subsection / subparagraph",
         "然后呢，又发现了一篇文章叫：",
-        "<a href='https://thecontentauthority.com/blog/chapter-vs-subchapter'>“Chapter vs Subchapter: When to Opt for One Term Over Another”</a>",
+        "<a target='_blank' href='https://thecontentauthority.com/blog/chapter-vs-subchapter'>“Chapter vs Subchapter: When to Opt for One Term Over Another”</a>",
         "不知道都说了啥。因为很长。",
         "然而最难区分的我觉得是 subchapter 和 section。",
         "它们肯定是有本质上的区别，否则不会换一个词。",
@@ -32,5 +32,5 @@ loglist.push({
 
 readlist.push({
     'title':'The Limits of Art',
-    'log_content':"./data/html/the_limits_of_art.html"
+    'log_content':"/data/html/the_limits_of_art.html"
 }) 
--- func/main.js
+++ func/main.js
@@ -1,3 +1,4 @@
+let siteTitle = "极客II艺术"
 
 var create_sth = function(elm,pElm,id,ih,clslst){
     var oItem = document.createElement(elm);
@@ -22,12 +23,11 @@ function page_main(){
     
     let oDIV_main = create_sth("div",document.body,"main","","maincss");
 
-    let oH1_logo = create_sth("h1",oDIV_main,"logo","极客与艺术","logocss");
+    let oH1_logo = create_sth("h1",oDIV_main,"logo",siteTitle,"logocss");
 
     let oUL_func_nav = create_sth("ul",oDIV_main,"ul_func_nav","","navcss");
     let oLI_navbtn_read = create_sth("li",oUL_func_nav,"oLI_navbtn_read","<a>reading</a>","btncss");
     let oLI_navbtn_thinking = create_sth("li",oUL_func_nav,"oLI_navbtn_thinking","<a>thinking</a>","btncss");
-    console.log(typeof(oLI_navbtn_thinking))
     let oLI_navbtn_blog = create_sth("li",oUL_func_nav,"oLI_navbtn_blog","<a>log</a>","btncss");
     let oLI_navbtn_and = create_sth("li",oUL_func_nav,"oLI_navbtn_and","<small>and ...how 2</small>","btncss");
     let oLI_navbtn_tao = create_sth("li",oUL_func_nav,"oLI_navbtn_tao","<a>tao</a>","btncss");
